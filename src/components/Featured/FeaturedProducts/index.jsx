9326cbdc4baec34b6c0ccc039bcd9358efd6b352
afrah8 <afrahabualrob8@gmail.com>
2022-12-08 01:22:18 +0200

    select featured product

--- src/components/Featured/FeaturedProducts/index.jsx
+++ src/components/Featured/FeaturedProducts/index.jsx
@@ -5,10 +5,14 @@ import { useShoppingCart } from "../../../Context/ShoppingCartContext";
 
 const FeaturedProducts = () => {
   const { productsJson } = useShoppingCart();
+  const featuredProducts = productsJson
+    .sort((p1, p2) => p2.rating.rate - p1.rating.rate)
+    .filter((product, index) => index < 8);
+
   return (
     <div className={styles.featuredProducts}>
       {[] &&
-        productsJson.map((product) => {
+        featuredProducts.map((product) => {
           const { id } = product;
           return (
             <div key={id} className={styles.featuredProduct_item}>
